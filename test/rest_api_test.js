166e228ba9096bc58dd3904522e68c89dff2255c
runonwalls <hmorgan471@gmail.com>
2015-05-24 21:39:43 -0700

    add synchronous auth

--- test/rest_api_test.js
+++ test/rest_api_test.js
@@ -10,25 +10,39 @@ var Rabbit = require('../lib/models/models.js').Rabbit;
 chai.use(chaiHttp);
 
 var server = require('../lib/server.js'); //run our server
+var token; //(this should definitely not be out here and i am sorry)
 
 describe('Single-Resource REST API', function() {
 
   before(function(done) {
-    server.once('started', function() {
-      done();
-    });
+    //server.once('started', function() { //Unnecessary, auth_test will be run first
+    done();
+    //});
   });
   after(function(done) {
-    mongoose.connection.db.dropDatabase(function() {
-      //mongoose.connection.db.emit('first tests done');
-      done();
-    });
+    //mongoose.connection.db.dropDatabase(function() {
+    //mongoose.connection.db.emit('first tests done');
+    done();
+    //});
+  });
+
+  it('should be able to sign in a user', function(done) {
+    chai.request('localhost:3000')
+      .get('/sign_in')
+      .auth('bleh@example.com', 'foobar123')
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res.status).to.eql(200);
+        expect(res.body).to.have.property('token');
+        token = res.body.token;
+        done();
+      });
   });
 
   it('should be able to add a new rabbit', function(done) {
     chai.request('localhost:3000')
       .post('/rabbits')
-      .send({name: 'Shrubs', weight: 10})
+      .send({eat: token, name: 'Shrubs', weight: 10})
       .end(function(err, res) {
         expect(err).to.eql(null);
         expect(res.status).to.eql(200);
@@ -41,6 +55,7 @@ describe('Single-Resource REST API', function() {
   it('should be able to get an array of all rabbits', function(done) {
     chai.request('localhost:3000')
       .get('/rabbits')
+      .send({eat: token})
       .end(function(err, res) {
         expect(err).to.eql(null);
         expect(res.status).to.eql(200);
@@ -70,7 +85,7 @@ describe('REST API continued', function() {
   it('should be able to update a rabbit', function(done) {
     chai.request('localhost:3000')
       .put('/rabbits')
-      .send({name: 'Buckles 2', weight: 11, _id: testRabbit._id}) //that got dark quickly!
+      .send({eat: token, name: 'Buckles 2', weight: 11, _id: testRabbit._id}) //that got dark quickly!
       .end(function(err, res) {
         expect(err).to.eql(null);
         expect(res.status).to.eql(200);
@@ -82,7 +97,7 @@ describe('REST API continued', function() {
   it('should be able to delete a rabbit', function(done) {
     chai.request('localhost:3000')
       .delete('/rabbits')
-      .send({_id: testRabbit._id}) //(Buckles 2 just wasn't the same as Buckles 1)
+      .send({eat: token, _id: testRabbit._id}) //(Buckles 2 just wasn't the same as Buckles 1)
       .end(function(err, res) {
         expect(err).to.eql(null);
         expect(res.status).to.eql(200);
