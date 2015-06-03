d29d4a560d69de967ce14e41d323b72e25986dd5
runonwalls <hmorgan471@gmail.com>
2015-06-03 14:37:26 -0700

    add client side with simple html page, bundle, and test

--- app/js/client.js
+++ app/js/client.js
@@ -1,14 +1,10 @@
 'use strict';
 
 //webpack's entry point
-
-//var angular; //will be assigned a value by requiring angular/angular.js.
-             //(without this, jshint won't like angular suddenly being defined.)
-
 //require('angular/angular'); //run the angular.js file inside angular, NOT index.js inside angular
                             //(this way we get everything in angular.js within this file's scope)
 
-//Simpler, but keeping the old notes around for now:
+//This avoids a jshint error where angular is undefined, but keeping the old notes around for now:
 var angular = require('angular');
 
 var greet = require('./greet');
